f9712ad31e290bfa4b8ea4582188923944fe3ede
Kelvin Muyambo <kelvinm@afrosoft.co.zw>
2018-10-03 15:10:46 +0200

    commit displaying event details

--- src/components/event-item-details/event-item-details.ts
+++ src/components/event-item-details/event-item-details.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavParams } from 'ionic-angular';
+import { FirebaseServiceProvider } from '../../providers/firebase-service';
+import { ListingImage } from '../../model/listing-image';
 
 @Component({
   selector: 'event-item-details',
@@ -7,7 +9,16 @@ import { NavParams } from 'ionic-angular';
 })
 export class EventItemDetailsComponent {
   event: any;
-  constructor(params: NavParams) {
+  images: Array<any> = [];
+  constructor(params: NavParams, private firebaseService: FirebaseServiceProvider) {
     this.event = params.data.event;
+    this.loadImages();
+  }
+
+  loadImages() {
+    this.event.images.forEach(image => {
+      this.firebaseService.getNoLoad('/event-image/' + image.data, (r) =>
+        this.images.push(new ListingImage(r.toString())))
+    })
   }
 }
--- src/components/event-item/event-item.ts
+++ src/components/event-item/event-item.ts
@@ -1,18 +1,23 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { FirebaseServiceProvider } from '../../providers/firebase-service';
+import { Event } from '../../model/event';
 
 @Component({
   selector: 'event-item',
   templateUrl: 'event-item.html'
 })
 export class EventItemComponent {
-  @Input() event: any;
+  @Input() event: Event;
   @Output() result = new EventEmitter<any>();
   icon: string;
 
   constructor(private firebaseSvc: FirebaseServiceProvider) {
   }
-  
+
+  ngOnInit(): void {
+    this.getIcon();
+  }
+
   getIcon() {
     this.firebaseSvc.getNoLoad('/event-image/' + this.event.image, (icon) => {
       this.icon = icon.toString();
